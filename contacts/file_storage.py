07780de0e5fbbbb2168890f20b7c265da1d45759
Davide Bigotti <bigotti.davide@hotmail.com>
2024-09-15 14:19:57 +0200

    implement file storage

--- contacts/file_storage.py
+++ contacts/file_storage.py
@@ -1,20 +1,20 @@
 from typing import Sequence
 from contacts.core import Person
 
-fields_mapping = {
-        "name": 0,
-        "surname": 1,
-        "address": 2,
-        "telephone": 3,
-        "age": 4,
-}
+
+_filename = "info.txt"
+
+
+def _as_string(p: Person):
+    age = p.age if p.age else ""
+    return f"{p.name};{p.surname};{p.address};{p.telephone};{age}\n"
 
 
 def retrieve_all() -> Sequence[Person]:
     contacts = []
-    with open("informazioni.txt", "r") as file:
+    with open(_filename, "r") as file:
         for line in file:
-            # strip trailing '\n'
+            # strip trailing '\n' or ';' in case age is not evaluated
             fields = line[:-1].split(";")
             p = Person(fields[0], fields[1], fields[2], fields[3], fields[4])
             contacts.append(p)
@@ -22,12 +22,25 @@ def retrieve_all() -> Sequence[Person]:
 
 
 def insert(p: Person) -> None:
-    return
+    with open(_filename, "a") as file:
+        file.write(_as_string(p))
 
 
-def update(old: Person, new: Person):
-    return
+def update(old: Person, new: Person) -> None:
+    with open(_filename, "r+") as file:
+        lines = file.readlines()
+        file.seek(0)
+        i = lines.index(_as_string(old))
+        lines[i] = _as_string(new)
+        file.truncate()
+        file.writelines(lines)
 
 
 def delete(p: Person) -> None:
-    return
+    with open(_filename, "r+") as file:
+        lines = file.readlines()
+        file.seek(0)
+        i = lines.index(_as_string(p))
+        lines.pop(i)
+        file.truncate()
+        file.writelines(lines)
